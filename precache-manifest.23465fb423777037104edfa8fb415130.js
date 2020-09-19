b0bfab1f785007458881f87311d3906359b6a6c5
Kasper <kasper@firle.se>
2020-09-19 18:35:24 +0200

    Updates

---
+++ precache-manifest.23465fb423777037104edfa8fb415130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e57699c4f0f5bea6117156efbcbcef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6efbd7c23c5495746e67",
+    "url": "./static/css/main.860320fd.chunk.css"
+  },
+  {
+    "revision": "1a0322ec1e937fc1c731",
+    "url": "./static/js/2.56e8ab34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.56e8ab34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6efbd7c23c5495746e67",
+    "url": "./static/js/main.6ed88fee.chunk.js"
+  },
+  {
+    "revision": "6419d3a5b6e6d9dbae1c",
+    "url": "./static/js/runtime-main.c4ec8220.js"
+  }
+]);
